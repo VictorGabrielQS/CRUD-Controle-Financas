071afc771176658e5e9af3df9ee39436940fadd4
VictorGabrielQS <victor.git24@gmail.com>
2025-09-10 06:13:00 -0300

    Primeiro Commit

---
+++ CRUD-Controle-Finanças/src/main/java/com/VictorDevCode/CRUD_Controle_Financas/CrudControleFinancasApplication.java
@@ -0,0 +1,13 @@
+package com.VictorDevCode.CRUD_Controle_Financas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudControleFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudControleFinancasApplication.class, args);
+	}
+
+}
---
+++ CRUD-Controle-Finanças/src/test/java/com/VictorDevCode/CRUD_Controle_Financas/CrudControleFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VictorDevCode.CRUD_Controle_Financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudControleFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
